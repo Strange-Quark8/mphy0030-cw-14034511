8e730c193e01db7b655e952536b5bc5141160a45
Strange-Quark8 <57302623+Strange-Quark8@users.noreply.github.com>
2021-01-24 14:23:38 +0800

    gradient descent logic

---
+++ part1/task3/gradient_descent.m
@@ -0,0 +1,22 @@
+function [outputArg1,outputArg2] = gradient_descent(inputArg1,inputArg2)
+%GRADIENT_DESCENT Summary of this function goes here
+%   Detailed explanation goes here
+
+%command function; theta
+%start function; theta_null
+%alpha: step size
+%nabla: gradient of theta (largest change)
+
+%iterate:
+%1. initialise current position as x vector
+%2. subtract alpha*nabla
+%3. initialise solution as new theta_null
+
+%break clause: 
+%1. if iteration limit reached
+%2. if theta null = theta 
+%3. if abs(theta null - theta) >= tolerance level
+
+
+end
+
