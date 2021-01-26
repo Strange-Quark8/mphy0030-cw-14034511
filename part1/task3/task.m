c0f5e238ab77d021d972064ab0b1c0ee3af2c411
Strange-Quark8 <57302623+Strange-Quark8@users.noreply.github.com>
2021-01-26 12:44:55 +0800

    Need to Optimise & Validate

--- part1/task3/task.m
+++ part1/task3/task.m
@@ -11,7 +11,22 @@ a= randi(10,[10,1]); %range 1-10
 x= randi(10, [3,1]); %range 1-10
 
 % Current function solution
-f = (a(2)*x(1)^2) + (a(3)*x(2)^2)+(a(4)*x(3)^2)+(a(5)*x(1)*x(2))+(a(6)*x(1)*x(3))+(a(7)*x(2)*x(3))+(a(8)*x(1))+(a(9)*x(2))+(a(10)*x(3))+a(1);
+%f = (a(2)*x(1)^2) + (a(3)*x(2)^2)+(a(4)*x(3)^2)+(a(5)*x(1)*x(2))+(a(6)*x(1)*x(3))+(a(7)*x(2)*x(3))+(a(8)*x(1))+(a(9)*x(2))+(a(10)*x(3))+a(1);
 
-%partial diff of cost function
-%update equations
+%% Computing Multivariate 3D Polynomial on initial parameters
+Qpoly = quadratic_polynomial(a,x);
+
+%% Minimisation of fixed function via gradient descent
+%selection of gradient descent parameters
+% NOTE: Still need to optimise..
+theta_null = x; %initial pos vector
+alpha = 0.3; %step size
+epochs = 100; %iterations
+margin = 0.1 % tolerance level
+nabla = finite_difference_gradient(a,x); %initial vector gradient
+
+%optimised gradient descent solution
+theta = gradient_descent(a,theta_null,alpha,epochs,margin,nabla)
+
+
+%% Validation of descent?
